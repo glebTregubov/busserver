9d10634a850179cc892df2067377df8fc464664b
glebTregubov <gleb.tregubov@gmail.com>
2014-03-05 19:43:36 +0000

    start development

---
+++ db/db_config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 05.03.14
+ * Time: 20:58
+ */
+
+define('DB_USER', "bus"); //логин админа БД
+define('DB_PASSWORD', "bus"); // пароль админа БД
+define('DB_DATABASE', "buslocation"); // база данных
+define('DB_SERVER', "localhost"); // сервер 'localhost'
+?>
